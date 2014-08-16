aca078ff17cffea040887f9c35b17b365d657a7e
Johannes Wilm <johannes@wilm.no>
2014-08-16 08:14:59 +0100

    js cleanup + remove map attribution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,85 +1,87 @@
 'use strict';
 
 angular
-  .module('sedApp', [
-    'config',
-    'ngSanitize',
-    'ngRoute',
-    'ngResource',
-    'ngStorage',
-    'ngTable',
-    'nvd3ChartDirectives',
-    'ui.bootstrap',
-    'ngCsv'
-  ])
-  .config(function($httpProvider, $routeProvider, paginationConfig) {
+    .module('sedApp', [
+        'config',
+        'ngSanitize',
+        'ngRoute',
+        'ngResource',
+        'ngStorage',
+        'ngTable',
+        'nvd3ChartDirectives',
+        'ui.bootstrap',
+        'ngCsv'
+    ])
+    .config(function($httpProvider, $routeProvider, paginationConfig) {
 
-    paginationConfig.maxSize = 6;
-    paginationConfig.boundaryLinks = true;
-    paginationConfig.previousText = "<";
-    paginationConfig.nextText = ">";
-    paginationConfig.firstText = "<<";
-    paginationConfig.lastText = ">>";
+        paginationConfig.maxSize = 6;
+        paginationConfig.boundaryLinks = true;
+        paginationConfig.previousText = '<';
+        paginationConfig.nextText = '>';
+        paginationConfig.firstText = '<<';
+        paginationConfig.lastText = '>>';
 
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'HomeCtrl'
-      })
-      .when('/map', {
-        templateUrl: 'views/map.html',
-        controller: 'MapCtrl'
-      })
-      .when('/follow-ups', {
-        templateUrl: 'views/follow-ups.html',
-        controller: 'FollowUpsCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/map', {
+                templateUrl: 'views/map.html',
+                controller: 'MapCtrl'
+            })
+            .when('/follow-ups', {
+                templateUrl: 'views/follow-ups.html',
+                controller: 'FollowUpsCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
 
-    // Intercept 401s and redirect user to login
-    $httpProvider.interceptors.push([
-      '$rootScope', '$q', '$location', 'SETTINGS', function($rootScope, $q, $location, SETTINGS) {
-        return {
-          'request': function(config) {
-            if (config.url.substr(0, SETTINGS.formHubUrl.length) === SETTINGS.formHubUrl) {
-              var username = $rootScope.currentUser ? $rootScope.currentUser.username : '';
-              var password = $rootScope.currentUser ? $rootScope.currentUser.password : '';
+        // Intercept 401s and redirect user to login
+        $httpProvider.interceptors.push([
+            '$rootScope', '$q', '$location', 'SETTINGS',
+            function($rootScope, $q, $location, SETTINGS) {
+                return {
+                    'request': function(config) {
+                        if (config.url.substr(0, SETTINGS.formHubUrl.length) === SETTINGS.formHubUrl) {
+                            var username = $rootScope.currentUser ? $rootScope.currentUser.username : '';
+                            var password = $rootScope.currentUser ? $rootScope.currentUser.password : '';
 
-              config.headers['Authorization'] = 'Basic ' + btoa(username + ':' + password);
+                            config.headers.Authorization = 'Basic ' + btoa(username + ':' + password);
+                        }
+
+                        return config;
+                    },
+                    'responseError': function(response) {
+                        switch (response.status) {
+                            case 401:
+                                if ($location.path() !== '/login') {
+                                    $location.search('back', $location.path()).path('/login');
+                                }
+                                break;
+                        }
+
+                        return $q.reject(response);
+                    }
+                };
             }
+        ]);
+    })
+    .run(function($rootScope, $route, SETTINGS, Auth) {
+        $rootScope.SETTINGS = SETTINGS;
 
-            return config;
-          },
-          'responseError': function(response) {
-            switch (response.status) {
-              case 401:
-                if ($location.path() != '/login')
-                  $location.search('back', $location.path()).path('/login');
-                break;
-            }
-
-            return $q.reject(response);
-          }
+        $rootScope.logout = function() {
+            Auth.logout();
+            $route.reload();
         };
-      }
-    ]);
-  })
-  .run(function($rootScope, $route, SETTINGS, Auth) {
-    $rootScope.SETTINGS = SETTINGS;
-
-    $rootScope.logout = function() {
-      Auth.logout();
-      $route.reload();
-    };
-  })
-  .controller('NavBar', function($scope, $location) {
-    $scope.isActive = function(url) {
-      return url == $location.path();
-    }
-  });
+    })
+    .controller('NavBar', function($scope, $location) {
+        $scope.isActive = function(url) {
+            return url == $location.path();
+        }
+    });
