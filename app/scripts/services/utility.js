28d815554a8d919bd3ca127194e3d830a6536492
Justin <futuresandwich@users.noreply.github.com>
2014-08-17 12:51:55 +0100

    Update utility.js
    fragility hotfix

--- app/scripts/services/utility.js
+++ app/scripts/services/utility.js
@@ -11,7 +11,7 @@ angular.module('sedApp')
   .service('utility', function utility() {
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.toTitleCase = function(str){
-      return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      return (str||"").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     };
 
   });
