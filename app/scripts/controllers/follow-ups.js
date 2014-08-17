eee901cbff53d2bc6cf78a6c1b9a41f02fa789b3
Haykel Ben Jemia <hbj@allmas.tn>
2014-08-17 14:00:42 +0100

    Use data loader for follow ups table.

--- app/scripts/controllers/follow-ups.js
+++ app/scripts/controllers/follow-ups.js
@@ -1,9 +1,7 @@
 'use strict';
 
 angular.module('sedApp')
-  .controller('FollowUpsCtrl', function($scope, $filter, ngTableParams, aggregatedData) {
-
-    var RELOAD_DELAY = 300000;
+  .controller('FollowUpsCtrl', function($rootScope, $scope, $filter, ngTableParams, dataLoader) {
     $scope.MAX_TEMP = 38;
     $scope.csvHeader = [
       'Name',
@@ -30,7 +28,7 @@ angular.module('sedApp')
     ];
 
     function getFileName() {
-      return 'follow-ups-'+ $filter('date')(new Date(), 'yyyy-MM-dd hh-mm-ss');
+      return 'follow-ups-' + $filter('date')(new Date(), 'yyyy-MM-dd hh-mm-ss');
     }
 
     $scope.fileName = getFileName();
@@ -38,74 +36,30 @@ angular.module('sedApp')
       $scope.fileName = getFileName();
     };
 
-    var data = [];
-    $scope.csvData = data;
-    var loading = false;
     var locals = $scope.locals = {
-      loading: true,
-      error: false,
       totalItems: 0,
       currentPage: 1,
       tableParams: new ngTableParams({
         page: 1,
         count: 10,
         sorting: {
-          time: 'desc',
-        },
+          time: 'desc'
+        }
       }, {
         total: 0,
         counts: [],
         getData: function($defer, params) {
-          loading = true;
-          if (data.length)
-            resolve(data);
-          else {
+          var data = dataLoader.mergedData();
+          $scope.csvData = data;
 
-            aggregatedData.mergedData()
-              .then(function(merged) {
-                data = merged;
-                $scope.csvData = data;
-                resolve(data);
-              })
-              .catch(function() {
-                data = [];
-                resolve(data);
-                locals.error = true;
-              })
-              .finally(function() {
-                loading = false;
-                locals.loading = false;
-              });
-
-          }
-
-          function resolve(data) {
-            var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
-            locals.totalItems = orderedData.length;
-            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-          }
+          var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+          locals.totalItems = orderedData.length;
+          $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
       })
     };
 
-    setInterval(function() {
-
-      if (!loading) {
-        loading = true;
-        aggregatedData.mergedData()
-          .then(function(merged) {
-            data = merged;
-            $scope.csvData = data;
-            locals.tableParams.reload();
-          })
-          .catch(function(reason) {
-            console.error(reason);
-          })
-          .finally(function() {
-            loading = false;
-          });
-      }
-
-    }, RELOAD_DELAY);
-
+    $rootScope.$on('dataUpdated', function() {
+      locals.tableParams.reload();
+    });
   });
