5aca1bc995f9eff829e600893e2bca82a25d4c86
Haykel Ben Jemia <hbj@allmas.tn>
2014-08-17 13:58:35 +0100

    Display current time, last update time and loading status on nav bar.

---
+++ app/scripts/directives/current-time.js
@@ -0,0 +1,31 @@
+angular.module('sedApp')
+  .directive('currentTime', [
+    '$interval', 'dateFilter', function($interval, dateFilter) {
+      function link(scope, element, attrs) {
+        var format,
+          timeoutId;
+
+        function updateTime() {
+          element.text(dateFilter(new Date(), format));
+        }
+
+        scope.$watch(attrs.currentTime, function(value) {
+          format = value;
+          updateTime();
+        });
+
+        element.on('$destroy', function() {
+          $interval.cancel(timeoutId);
+        });
+
+        // start the UI update process; save the timeoutId for canceling
+        timeoutId = $interval(function() {
+          updateTime(); // update DOM
+        }, 1000);
+      }
+
+      return {
+        link: link
+      };
+    }
+  ]);
