c77964a3298ef8fc84ec73c9b69e6375b7bc1ad4
Tom Vincent <git@tlvince.com>
2014-08-20 13:48:40 +0100

    Add help-block test

--- test/e2e/login.js
+++ test/e2e/login.js
@@ -13,14 +13,26 @@ describe('SED', function() {
   });
 
   describe('login page', function() {
+    var form;
+
     beforeEach(function() {
       browser.get('/#/login');
+      form = element(by.tagName('form'));
     });
 
     it('should disable submit if username/password are missing', function() {
-      var form = element(by.tagName('form'));
       var submitButton = form.element(by.tagName('button'));
       expect(submitButton.isEnabled()).toBe(false);
+    });
+
+    it('should only should help if user has typed', function() {
+      var username = form.element(by.id('username'));
+      var helpBlocks = username.all(by.css('.help-block'));
+      expect(helpBlocks.count()).toBe(0);
+      var input = username.element(by.tagName('input'));
+      input.sendKeys('test');
+      input.clear();
+      expect(helpBlocks.count()).toBe(1);
     });
   });
 });
