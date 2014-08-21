7a9034039b5ee3502f6d6e4a942fde7b392d81da
Johannes Wilm <johannes@wilm.no>
2014-08-21 15:51:08 +0200

    only reload when error wasn't a 401, part2

--- app/scripts/services/data-loader.js
+++ app/scripts/services/data-loader.js
@@ -102,7 +102,7 @@ angular.module('sedApp')
           console.log(err);
           error = err;
           $rootScope.$emit('endLoad', err);
-          if (!(err.status && err.status==401) {
+          if (!(err.status && err.status===401)) {
             timeout = $timeout(load, ERROR_RELOAD_DELAY);
           }
         })
